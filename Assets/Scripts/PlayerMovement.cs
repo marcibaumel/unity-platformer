c157285d5b6d24fe14c34b807fc740c560bc88d2
Márton Baumel <marcibaumel@gmail.com>
2025-05-04 16:47:13 +0200

    assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,14 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public float movementSpeed = 5.0f;
     public float horizontalMovement;
+    public float jumpPower = 10.0f;
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
+    public LayerMask groundLayer;
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -16,7 +21,38 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocity = new Vector2(horizontalMovement * movementSpeed, rb.linearVelocityY);
     }
 
-    public void Move(InputAction.CallbackContext context){
+    public void Move(InputAction.CallbackContext context)
+    {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded())
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpPower);
+            }
+            else if (context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, rb.linearVelocityY * 0.5f);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            return true;
+        }
+        return false;
     }
 }
